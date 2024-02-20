4f48d3ecebf40e333ec0b5990869d45669071982
Smzh <129963508+HunterShenSmzh@users.noreply.github.com>
2024-02-21 01:27:07 +0800

    重新整理所有内容，方便后续更新

---
+++ Translate script(TGW env use)/启动自动翻译.bat
@@ -0,0 +1,88 @@
+@echo off
+chcp 65001
+cd /D "%~dp0"
+set PATH=%PATH%;%SystemRoot%\system32
+echo "%CD%"| findstr /C:" " >nul && echo 路径中不能有空格 && goto end
+set TMP=..\text-generation-webui\installer_files
+set TEMP=..\text-generation-webui\installer_files
+(call conda deactivate && call conda deactivate && call conda deactivate) 2>nul
+set CONDA_ROOT_PREFIX=..\text-generation-webui\installer_files\conda
+set INSTALL_ENV_DIR=..\text-generation-webui\installer_files\env
+set PYTHONNOUSERSITE=1
+set PYTHONPATH=
+set PYTHONHOME=
+set "CUDA_PATH=%INSTALL_ENV_DIR%"
+set "CUDA_HOME=%CUDA_PATH%"
+call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%" || ( echo. && echo Miniconda hook not found. && goto end )
+
+echo 正在运行 自动翻译 脚本...
+echo 使用kaggle平台在线翻译选1，本地翻译选2 
+set /p platform="请输入使用的翻译平台(kaggle输入1，本地输入2)："
+if /i "%platform%"=="1" goto online
+if /i "%platform%"=="2" goto local
+goto invalid
+
+:online
+set /p URL="请输入URL地址:"
+echo 翻译日语请使用Sakura模型，翻译英语请使用Qwen模型 
+set /p language="请输入需要被翻译的主要语言 (日语输入1，英语输入2)："
+if /i "%language%"=="1" goto jp_online
+if /i "%language%"=="2" goto en_online
+goto invalid
+
+:local
+echo 翻译日语请使用Sakura模型，翻译英语请使用Qwen模型 
+set /p language="请输入需要被翻译的主要语言 (日语输入1，英语输入2)："
+if /i "%language%"=="1" goto jp_local
+if /i "%language%"=="2" goto en_local
+goto invalid
+
+:jp_local
+echo 正在运行translate_jp脚本...
+cd %~dp0
+python translate_jp.py http://127.0.0.1:5000
+if errorlevel 1 (
+    goto :end
+)
+echo 请务必检查翻译完成.json内的内容 
+goto :end
+
+:en_local
+echo 正在运行translate_en脚本...
+cd %~dp0
+python translate_en.py http://127.0.0.1:5000
+if errorlevel 1 (
+    goto :end
+)
+echo 请务必检查翻译完成.json内的内容 
+goto :end
+
+:jp_online
+echo 正在运行translate_kaggle_jp脚本...
+cd %~dp0
+python translate_kaggle_jp.py %URL%
+if errorlevel 1 (
+    goto :end
+)
+echo 请务必检查翻译完成.json内的内容 
+goto :end
+
+:en_online
+echo 正在运行translate_kaggle_en脚本...
+cd %~dp0
+python translate_kaggle_en.py %URL%
+if errorlevel 1 (
+    goto :end
+)
+echo 请务必检查翻译完成.json内的内容 
+goto :end
+
+:invalid
+echo 无效的输入
+echo 请重新输入有效的内容
+pause>nul
+goto start
+
+:end
+pause
+
